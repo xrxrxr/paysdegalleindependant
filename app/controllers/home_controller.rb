f33af41be71f7144632fcfbb2b6a18e2272424c4
JulienTHP <jpelisson@hotmail.fr>
2019-03-04 17:03:47 +0100

    Je suis un étron

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,10 @@
 class HomeController < ApplicationController
 	def index
-		def index
-			@products = Product.all
-			@categories = Category.all
-			respond_to do |format|
-				format.html
-				format.js
-			end
+		@products = Product.all
+		@categories = Category.all
+		respond_to do |format|
+			format.html
+			format.js
 		end
 	end
 end
