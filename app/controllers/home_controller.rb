9e75e773c58b6095f019c8b15d4ff058d88aacfc
zekidok <zekidok@gmail.com>
2019-03-04 17:38:51 +0100

    julien =

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,10 @@
 class HomeController < ApplicationController
+    def index
+        @products = Product.all
+        @categories = Category.all
+        respond_to do |format|
+            format.html
+            format.js
+        end
+    end
 end
