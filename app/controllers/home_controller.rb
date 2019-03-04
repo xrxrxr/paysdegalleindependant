09703a3c1ea45237317c4a5cc2509d548c9bc799
JulienTHP <jpelisson@hotmail.fr>
2019-03-04 14:58:11 +0100

    tentative infructueuse d'index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,12 @@
 class HomeController < ApplicationController
+	def index
+		def index
+			@products = Product.all
+			@categories = Category.all
+			respond_to do |format|
+				format.html
+				format.js
+			end
+		end
+	end
 end
