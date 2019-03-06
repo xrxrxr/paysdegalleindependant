eb0aab3956a7fa4b1b361976bb4eb3a0db19c928
JulienTHP <jpelisson@hotmail.fr>
2019-03-06 17:29:32 +0100

    Failed to write test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,17 +2,18 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
-  end
+  describe "GET #user" do
+    it "assign @user" do 
+      @user = User.create
 
-  describe "GET #show" do
-    it "returns http success" do
+      get :user, first_name: user.first_name
+      get :user, last_name: @user.last_name
+      expect(assigns(:user).to eq(@user))
+    end
+    
+    it "render the show template" do
       get :show
-      expect(response).to have_http_status(:success)
+      expect(response).to render_template("users/show")
     end
   end
 
