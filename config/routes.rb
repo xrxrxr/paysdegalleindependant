d77c1d3a76685c82fed8beee6ad58f7b9928e118
zekidok <zekidok@gmail.com>
2019-03-06 09:07:11 +0100

    bonsoir

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'avatars/create'
   devise_for :users
   root to: "home#index"
   
-  resources :users, only: [:show, :update, :edit, :destroy]
-  
+  resources :users, only: [:show, :update, :edit, :destroy] do
+    resources :avatars, only: [:create]
+  end
+
   resources :products do
     resources :cart_products, only: [:create, :update, :destroy]
   end
