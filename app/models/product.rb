16fc806287e2f5fcc170bdef819dcabda9387bbc
deoladed <leo.robert.mk@gmail.com>
2019-03-06 17:58:01 +0100

    refacto controlelr views order cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,18 @@
 class Product < ApplicationRecord
-has_one_attached :cat_picts
-after_save :grab_image
+  has_one_attached :cat_picts
+  after_save :grab_image
 
   belongs_to :category
   has_many :cart_products, dependent: :destroy
   has_many :carts, through: :cart_products, source: :product
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, 
+    presence: true,
+    numericality: { greater_than_or_equal_to: 0 }
+  validates :pict_url, presence: true
+
 
   def grab_image    
     downloaded_image = (open(self.pict_url))
