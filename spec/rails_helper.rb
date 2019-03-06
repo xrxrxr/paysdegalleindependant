4a1bd2961f9e052412c7f9dd1a2126d27cced1ba
zekidok <zekidok@gmail.com>
2019-03-06 16:01:27 +0100

    Dans le cul RSPEC

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,8 +32,7 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  #config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.include FactoryBot::Syntax::Methods
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
@@ -59,16 +58,4 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-end
-
-
-    Shoulda::Matchers.configure do |config|
-      config.integrate do |with|
-        with.test_framework :rspec
-        with.library :rails
-      end
-    end
-
-    RSpec.configure do |config|
-  config.include Devise::Test::ControllerHelpers, type: :controller
 end
